24505e30cf14ccde0174042d32aa6c7fb066ffc1
Dan Leech <hi@danleech.com>
2015-10-15 14:30:13 +0100

    Finished sort algorithm

--- build.js
+++ build.js
@@ -15,6 +15,7 @@ for (var i = 0; i < source.icons.length; i++) {
     var max = Math.max(red, green, blue);
     var min = Math.min(red, green, blue);
     var delta = max - min;
+    source.icons[i].luminance = 100 * (max - min) / 2;
     if (delta === 0) {
         var hue = 0;
     } else {
@@ -33,9 +34,30 @@ for (var i = 0; i < source.icons.length; i++) {
 }
 
 // Sort icons by hue
+
+// Ensures blue icons appear first in the last, purple ones last
+for (var i = 0; i < source.icons.length; i++) {
+    source.icons[i].hue += 90;
+    source.icons[i].hue = source.icons[i].hue % 360;
+}
 source.icons.sort(function(a, b) {
     return parseFloat(b.hue) - parseFloat(a.hue);
 });
+var tmp = [];
+for (var i = 0; i < source.icons.length; i++) {
+    if (source.icons[i].luminance < 10) {
+        tmp.push(source.icons[i]);
+        source.icons.splice(i,1);
+        i--;
+    }
+}
+tmp.sort(function(a, b) {
+    return parseFloat(b.luminance) - parseFloat(a.luminance);
+});
+for (var i = 0; i < tmp.length; i++) {
+    source.icons.push(tmp[i]);
+}
+console.log(tmp);
 
 // Read header and footer content into variables
 var fs = require('fs');
@@ -51,10 +73,19 @@ var fs = require('fs');
 var header = fs.readFileSync('./src/header.html', 'utf8');
 var footer = fs.readFileSync('./src/footer.html', 'utf8');
 
-// Build tiles content
-var main = "";
+// Build content
+var main = "<p class=\"hero\">" + source.icons.length + " SVG icons for popular brands. Download them from <a href=\"https://github.com/danleech/simple-icons\">GitHub</a>.</p>\n<ul class=\"tiles\">";
+
 for (var i = 0; i < source.icons.length; i++) {
-    main += "<li class=\"tiles__item\" style=\"background-color:#" + source.icons[i].hex + "\">" + source.icons[i].title + "<br>#" + source.icons[i].hex + "</li>";
+    var filePath = source.icons[i].title.toLowerCase();
+    filePath = filePath.replace(' ', '');
+    filePath = filePath.replace('.', '');
+    filePath = filePath.replace('+', 'plus');
+    filePath = "./icons/" + filePath + ".svg";
+    console.log(source.icons[i].title + ", lum = " + source.icons[i].luminance);
+    var fs = require('fs');
+    var svg = fs.readFileSync(filePath, 'utf8');
+    main += "\t\t\t<li style=\"background-color:#" + source.icons[i].hex + "\">" + svg + source.icons[i].title + "<br><span class=\"hex\">#" + source.icons[i].hex + "</span></li>\n";
 }
 
 // Put all content together and export to index.html
