60551b179b3b054f3d103269fcc32977a15986b5
Dan Leech <hi@danleech.com>
2015-10-16 16:25:35 +0100

    Added Twitch

--- build.js
+++ build.js
@@ -43,7 +43,7 @@ for (var i = 0; i < source.icons.length; i++) {
 
 // Ensures blue icons appear first in the last, purple ones last
 for (var i = 0; i < source.icons.length; i++) {
-    source.icons[i].hue += 90;
+    source.icons[i].hue += 100;
     source.icons[i].hue = source.icons[i].hue % 360;
 }
 source.icons.sort(function(a, b) {
