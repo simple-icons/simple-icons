688e406e16a5cdce19c362b6493578897db92063
birjolaxew <johanringmann@gmail.com>
2018-08-05 19:27:54 +0200

    Added SVGLint

---
+++ scripts/svglintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    rules: {
+        elm: {
+            "svg": 1,
+            "svg > title": 1,
+            "g": false,
+        },
+        attr: [
+            { // ensure that the SVG elm has the appropriate attrs
+                "aria-labelledby": /^simpleicons-.+-icon$/,
+                "role": "img",
+                "viewBox": "0 0 24 24",
+                "xmlns": "http://www.w3.org/2000/svg",
+                
+                "rule::selector": "svg",
+                "rule::whitelist": true,
+            },
+            { // ensure that the title elm has the appropriate attr
+                "id": /^simpleicons-.+-icon$/,
+
+                "rule::selector": "svg > title",
+                "rule::whitelist": true,
+            }
+        ]
+    }
+};
