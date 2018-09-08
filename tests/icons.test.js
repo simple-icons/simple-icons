7343fb5794ee44f092fe379826e60eeed49805c5
Eric Cornelissen <ericornelissen@gmail.com>
2018-09-08 22:44:08 +0300

    Implement exhaustive tests of properties for each icon

---
+++ tests/icons.test.js
@@ -0,0 +1,24 @@
+const { icons } = require('../_data/simple-icons.json');
+const { titleToFilename } = require('../scripts/utils.js');
+
+icons.forEach(icon => {
+  const filename = titleToFilename(icon.title);
+  const subject = require(`../icons/${filename}.js`);
+
+  test(`${icon.title} has a "title"`, () => {
+    expect(typeof subject.title).toBe('string');
+  });
+
+  test(`${icon.title} has a "hex" value`, () => {
+    expect(typeof subject.hex).toBe('string');
+    expect(subject.hex).toHaveLength(6);
+  });
+
+  test(`${icon.title} has a "source"`, () => {
+    expect(typeof subject.source).toBe('string');
+  });
+
+  test(`${icon.title} has an "svg"`, () => {
+    expect(typeof subject.svg).toBe('string');
+  });
+});
