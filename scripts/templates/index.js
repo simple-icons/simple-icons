c5a0acfb59898a335257d9a272cf024cfbfd0f37
Eric Cornelissen <ericornelissen@gmail.com>
2019-07-13 12:09:43 +0100

    Add .get function to index.js export

--- scripts/templates/index.js
+++ scripts/templates/index.js
@@ -1,1 +1,17 @@
-module.exports=%s
+var icons = %s;
+
+module.exports = icons;
+module.exports.get = function(targetName) {
+  if (icons[targetName]) {
+    return icons[targetName];
+  } else {
+    var normalizedName = targetName.toLowerCase();
+    for (var iconName in icons) {
+      var icon = icons[iconName];
+      if ((icon.title && icon.title.toLowerCase() === normalizedName)
+       || (icon.name && icon.name === normalizedName)) {
+         return icon;
+      }
+    }
+  }
+}
