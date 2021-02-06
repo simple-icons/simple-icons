fb239d8bfe0375976d9138db28acec3f6eb962fa
Eric Cornelissen <ericornelissen@gmail.com>
2021-02-06 19:33:28 +0100

    Switch from jsonlint2 to jsonschema
    This gives is better support for advanced features of JSON schema.
    
    A couple of clarifications:
    - There does exist a jsonschema-cli package, but it is rather limited
      and crucially doens't exit with a non-zero exit code if there is an
      error. (it is also pretty old and not maintained), hence the custom
      script.
    - I renamed .jsonlintschema 1) for clarity (lint is no longer accurate)
      and 2) the .json extension allows easy imorting in the script and 3)
      it adds syntax highlighting.
    - The script outputs the number of errors in the end because the output
      gets pretty big pretty quickly, this way you can see it easily from
      your CLI.
    - We could customize how the errors are logged, but I feel that is
      beyond this PR.

---
+++ scripts/jsonlint.js
@@ -0,0 +1,19 @@
+const path = require("path");
+const Validator = require("jsonschema").Validator;
+
+const schemaFile = path.resolve(__dirname, "..", ".jsonschema.json");
+const dataFile = path.resolve(__dirname, "..", "_data", "simple-icons.json");
+
+const schema = require(schemaFile);
+const data = require(dataFile);
+
+var validator = new Validator();
+const result = validator.validate(data, schema);
+if (result.errors.length > 0) {
+  result.errors.forEach((error) => {
+    console.error(error);
+  });
+
+  console.error(`Found ${result.errors.length} error(s) in simple-icons.json`);
+  process.exit(1);
+}
