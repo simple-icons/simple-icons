f042a7af92fd8b796b454f1ad191435475cdb4d3
Álvaro Mondéjar <mondejar1994@gmail.com>
2021-02-20 16:38:46 +0100

    Upgrade SVGO to version 2

---
+++ svgo.config.js
@@ -0,0 +1,123 @@
+const { extendDefaultPlugins } = require('svgo');
+
+module.exports = {
+  multipass: true,
+  plugins: extendDefaultPlugins([
+    {
+      name: 'convertPathData',
+      params: {
+        // 3 decimals of precision in floating point numbers
+        floatPrecision: 3,
+        // Some editors (eg. Adobe Illustrator) does not parse flags without space wrapping
+        // https://github.com/simple-icons/simple-icons/commit/5e57e8fef98cffb8363f482ab0860c954328b236
+        noSpaceAfterFlags: false,
+      }
+    },
+
+    // Sort attributes on the <SVG>
+    {
+      name: 'sortAttrs',
+      params: {
+        order: ['role', 'viewBox'],
+        xmlnsOrder: 'end',
+      }
+    },
+
+    // Convert basic shapes (such as <circle>) to <path>, including <arc>
+    {
+      name: 'convertShapeToPath',
+      params: {
+        convertArcs: true
+      }
+    },
+
+    // Compound all <path>s into one
+    {
+      name: 'mergePaths',
+      params: {
+        force: true,
+        noSpaceAfterFlags: false,
+      }
+    },
+
+    // Keep the <title> tag
+    {
+      name: 'removeTitle',
+      active: false,
+    },
+
+    // Don't remove the role="img" attribute and automatically
+    // add it to the SVG if it's not
+    {
+      name: 'addAttributesToSVGElement',
+      params: {
+        attributes: [
+          {role: 'img'},
+        ]
+      }
+    },
+
+    // Keep the 'role' attribute, if is already defined
+    {
+      name: 'removeUnknownsAndDefaults',
+      params: {
+        keepRoleAttr: true,
+      }
+    },
+
+    // Remove all attributes except 'role', 'viewBox' and 'xmlns' of <svg>s
+    {
+      name: 'removeAttrs',
+      params: {
+        attrs: [
+          'baseProfile',
+          'version',
+          'fill-rule',
+        ],
+      }
+    },
+
+    // Remove paths with fill="none"
+    {
+      name: 'removeUselessStrokeAndFill',
+      params: {
+        removeNone: true,
+      }
+    },
+
+    // Explicitly enable everything else
+    {name: 'removeDoctype'},
+    {name: 'removeXMLProcInst'},
+    {name: 'removeComments'},
+    {name: 'removeMetadata'},
+    {name: 'removeEditorsNSData'},
+    {name: 'cleanupAttrs'},
+    {name: 'inlineStyles'},
+    {name: 'minifyStyles'},
+    {name: 'convertStyleToAttrs'},
+    {name: 'cleanupIDs'},
+    {name: 'prefixIds'},
+    {name: 'removeRasterImages'},
+    {name: 'removeUselessDefs'},
+    {name: 'cleanupNumericValues'},
+    {name: 'cleanupListOfValues'},
+    {name: 'convertColors'},
+    {name: 'removeNonInheritableGroupAttrs'},
+    {name: 'removeViewBox'},
+    {name: 'cleanupEnableBackground'},
+    {name: 'removeHiddenElems'},
+    {name: 'removeEmptyText'},
+    {name: 'moveElemsAttrsToGroup'},
+    {name: 'moveGroupAttrsToElems'},
+    {name: 'collapseGroups'},
+    {name: 'convertTransform'},
+    {name: 'removeEmptyAttrs'},
+    {name: 'removeEmptyContainers'},
+    {name: 'removeUnusedNS'},
+    {name: 'removeDesc'},
+    {name: 'removeDimensions'},
+    {name: 'removeStyleElement'},
+    {name: 'removeScriptElement'},
+    {name: 'removeOffCanvasPaths'},
+  ])
+};
