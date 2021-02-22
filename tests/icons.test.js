078ad4d714801bdcf22b3f44dd1949838fde4322
Álvaro Mondéjar <mondejar1994@gmail.com>
2021-02-22 17:20:07 +0100

    Ensure that icon paths start with moveto command (#5069)

--- tests/icons.test.js
+++ tests/icons.test.js
@@ -24,7 +24,6 @@ icons.forEach(icon => {
 
   test(`${icon.title} has a "path"`, () => {
     expect(typeof subject.path).toBe('string');
-    expect(subject.path).toMatch(/[MmZzLlHhVvCcSsQqTtAaEe0-9-,.\s]/g);
   });
 
   test(`${icon.title} has a "slug"`, () => {
