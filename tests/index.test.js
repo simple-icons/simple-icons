c67143ee2415e8936c6ec806a896efe366d4848d
Eric Cornelissen <ericornelissen@gmail.com>
2021-03-28 20:16:05 +0200

    Improve test names and test conditions

--- tests/index.test.js
+++ tests/index.test.js
@@ -6,33 +6,36 @@ icons.forEach(icon => {
   const name = icon.slug || icon.title;
   const subject = simpleIcons[name];
 
-  test(`${icon.title} has a "title"`, () => {
+  test(`${icon.title} has the correct "title"`, () => {
     expect(typeof subject.title).toBe('string');
+    expect(subject.title).toEqual(icon.title);
   });
 
-  test(`${icon.title} has a "hex" value`, () => {
+  test(`${icon.title} has the correct "slug"`, () => {
+    expect(typeof subject.slug).toBe('string');
+    expect(subject.slug).toEqual(getIconSlug(icon));
+  });
+
+  test(`${icon.title} has the correct "hex" value`, () => {
     expect(typeof subject.hex).toBe('string');
-    expect(subject.hex).toHaveLength(6);
+    expect(subject.hex).toEqual(icon.hex);
   });
 
-  test(`${icon.title} has a "source"`, () => {
+  test(`${icon.title} has the correct "source"`, () => {
     expect(typeof subject.source).toBe('string');
+    expect(subject.source).toEqual(icon.source);
   });
 
-  test(`${icon.title} has an "svg"`, () => {
+  test(`${icon.title} has an "svg" value`, () => {
     expect(typeof subject.svg).toBe('string');
   });
 
-  test(`${icon.title} has a "path"`, () => {
+  test(`${icon.title} has a valid "path" value`, () => {
     expect(typeof subject.path).toBe('string');
     expect(subject.path).toMatch(/^[MmZzLlHhVvCcSsQqTtAaEe0-9-,.\s]+$/g);
   });
 
-  test(`${icon.title} has a "slug"`, () => {
-    expect(typeof subject.slug).toBe('string');
-  });
-
-  test(`${icon.title} has ${icon.guidelines ? "" : "no"} "guidelines"`, () => {
+  test(`${icon.title} has ${icon.guidelines ? "the correct" : "no"} "guidelines"`, () => {
     if (icon.guidelines) {
       expect(typeof subject.guidelines).toBe('string');
     } else {
