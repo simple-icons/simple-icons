ae43df4cbc6aa490523c14507dde121d8091b1ff
Eric Cornelissen <ericornelissen@gmail.com>
2021-04-25 19:49:23 +0200

    Add script to bump major version in README
    On lines 29-32, we could alternatively use `replaceAll`, I opted not to
    as it is not yet(?) part of an LTS release of NodeJS.

---
+++ scripts/bump-cdn-version.js
@@ -0,0 +1,48 @@
+#!/usr/bin/env node
+/**
+ * @fileoverview
+ * Updates the CDN URLs in the README.md to the latest major version. Does
+ * nothing if the README.md is already up-to-date.
+ *
+ * It assumes the major version in the README is either equal to the current
+ * package major version, or the package minor version minus 1. If not the
+ * behaviour of this script should be consider undefined.
+ */
+
+const fs = require("fs");
+const path = require("path");
+const getMajorVersion = require("semver/functions/major");
+
+const rootDir = path.resolve(__dirname, "..");
+const PACKAGE_JSON_FILE = path.resolve(rootDir, "package.json");
+const README_FILE = path.resolve(rootDir, "README.md");
+
+function getManifest() {
+  const manifestRaw = fs.readFileSync(PACKAGE_JSON_FILE).toString();
+  const manifestJson = JSON.parse(manifestRaw);
+  return manifestJson;
+}
+
+function updateVersionInReadmeIfNecessary(majorVersion) {
+  let content = fs.readFileSync(README_FILE).toString();
+
+  content = content.replace(
+    new RegExp(`simple-icons@v${majorVersion - 1}`, "g"),
+    `simple-icons@v${majorVersion}`,
+  );
+
+  fs.writeFileSync(README_FILE, content);
+}
+
+function main() {
+  try {
+    const manifest = getManifest();
+    const majorVersion = getMajorVersion(manifest.version);
+    updateVersionInReadmeIfNecessary(majorVersion);
+  } catch (error) {
+    console.error("Failed to update CDN version number:", error);
+    process.exit(1);
+  }
+}
+
+main();
