7c8146e475ffaf3a0c77d1c5c88a42686ef2399b
Sachin Raja <sachinraja2349@gmail.com>
2021-11-08 02:55:47 -0800

    refactor: reorganize tests, reduce repetition (#6829)
    * refactor: reorganize tests, reduce repetition
    
    * fix conflicts
    
    * fix conflicts
    
    * remove unnecessary type checks and use toStrictEqual
    
    * move index tests to separate file
    
    * test svg values in testIcon

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,78 +1,9 @@
-const path = require('path');
-const fs = require('fs');
 const { icons } = require('../_data/simple-icons.json');
 const simpleIcons = require('../index.js');
-const { getIconSlug } = require('../scripts/utils.js');
-
-const iconsDir = path.resolve(__dirname, '..', 'icons');
+const { getIconSlug } = require('../scripts/utils');
 
 icons.forEach((icon) => {
   const slug = getIconSlug(icon);
-  const subject = simpleIcons[slug];
-  const svgPath = path.resolve(iconsDir, `${slug}.svg`);
-
-  test(`${icon.title} has the correct "title"`, () => {
-    expect(typeof subject.title).toBe('string');
-    expect(subject.title).toEqual(icon.title);
-  });
-
-  test(`${icon.title} has the correct "slug"`, () => {
-    expect(typeof subject.slug).toBe('string');
-    expect(subject.slug).toEqual(slug);
-  });
-
-  test(`${icon.title} has the correct "hex" value`, () => {
-    expect(typeof subject.hex).toBe('string');
-    expect(subject.hex).toEqual(icon.hex);
-  });
-
-  test(`${icon.title} has the correct "source"`, () => {
-    expect(typeof subject.source).toBe('string');
-    expect(subject.source).toEqual(icon.source);
-  });
-
-  test(`${icon.title} has a valid "svg" value`, () => {
-    expect(typeof subject.svg).toBe('string');
-    const svgFileContents = fs
-      .readFileSync(svgPath, 'utf8')
-      .replace(/\r?\n/, '');
-    expect(subject.svg.substring(subject.svg.indexOf('<title>'))).toEqual(
-      svgFileContents.substring(svgFileContents.indexOf('<title>')),
-    );
-  });
-
-  test(`${icon.title} has a valid "path" value`, () => {
-    expect(typeof subject.path).toBe('string');
-    expect(subject.path).toMatch(/^[MmZzLlHhVvCcSsQqTtAaEe0-9-,.\s]+$/g);
-  });
-
-  test(`${icon.title} has ${
-    icon.guidelines ? 'the correct' : 'no'
-  } "guidelines"`, () => {
-    if (icon.guidelines) {
-      expect(typeof subject.guidelines).toBe('string');
-      expect(subject.guidelines).toEqual(icon.guidelines);
-    } else {
-      expect(subject.guidelines).toBeUndefined();
-    }
-  });
-
-  test(`${icon.title} has ${
-    icon.license ? 'the correct' : 'no'
-  } "license"`, () => {
-    if (icon.license) {
-      expect(typeof subject.license).toBe('object');
-      expect(subject.license).toHaveProperty('type', icon.license.type);
-      if (icon.license.type === 'custom') {
-        expect(subject.license).toHaveProperty('url', icon.license.url);
-      } else {
-        expect(typeof subject.license.url).toBe('string');
-        expect(subject.license.url).toMatch(/^https?:\/\/[^\s]+$/);
-      }
-    } else {
-      expect(subject.license).toBeUndefined();
-    }
-  });
 
   test(`'Get' ${icon.title} by its slug`, () => {
     const found = simpleIcons.Get(slug);
