ce17b9daf35ad898a545de09d01ed19d9a3f6b2a
Sachin Raja <sachinraja2349@gmail.com>
2021-11-23 12:03:27 -0800

    add jsdoc deprecation tags

--- scripts/build/package.js
+++ scripts/build/package.js
@@ -98,16 +98,19 @@ data.icons.forEach((icon) => {
 
   // write the static .js file for the icon
   const jsFilepath = path.resolve(iconsDir, `${filename}.js`);
-  const message = `This way of importing icons has been deprecated in v6.0.0 and will no longer work from v7.0.0, use \\"const {${iconExportName}} = require('simple-icons/icons')\\" instead`;
+  const newImportMessage = `use "const {${iconExportName}} = require('simple-icons/icons')" instead`;
+  const message = JSON.stringify(
+    `This way of importing icons has been deprecated in v6.0.0 and will no longer work from v7.0.0, use ${newImportMessage}`,
+  );
   writeJs(
     jsFilepath,
-    `console.warn("warn - ${message}");module.exports=${iconObject};`,
+    `console.warn("warn -", ${message});module.exports=${iconObject};`,
   );
 
   const dtsFilepath = path.resolve(iconsDir, `${filename}.d.ts`);
   writeTs(
     dtsFilepath,
-    'declare const i:import("../alias").I;export default i;',
+    `/**@deprecated ${newImportMessage}*/declare const i:import("../alias").I;export default i;`,
   );
 
   // add object to the barrel file
