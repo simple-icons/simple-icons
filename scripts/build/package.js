7dcfd04e3d8127c570c7299f41bafc782a13f138
Sachin Raja <sachinraja2349@gmail.com>
2021-11-23 12:28:41 -0800

    Update scripts/build/package.js
    Co-authored-by: Álvaro Mondéjar <mondejar1994@gmail.com>

--- scripts/build/package.js
+++ scripts/build/package.js
@@ -98,7 +98,7 @@ data.icons.forEach((icon) => {
 
   // write the static .js file for the icon
   const jsFilepath = path.resolve(iconsDir, `${filename}.js`);
-  const newImportMessage = `use "const { ${iconExportName} } = require('simple-icons/icons');" instead`;
+  const newImportMessage = `"const { ${iconExportName} } = require('simple-icons/icons');" instead`;
   const message = JSON.stringify(
     `Imports like "const ${icon.slug} = require('simple-icons/icons/${icon.slug}');" have been deprecated in v6.0.0 and will no longer work from v7.0.0, use ${newImportMessage}`,
   );
