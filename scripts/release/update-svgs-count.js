e3e831e72a66aa01eddebdb345fcac432dae380b
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2021-12-07 13:12:17 +0100

    Automatically update milestone (SVG count) in README

---
+++ scripts/release/update-svgs-count.js
@@ -0,0 +1,39 @@
+#!/usr/bin/env node
+/**
+ * @fileoverview
+ * Replaces the SVG count milestone "Over <NUMBER> Free SVG icons..." located
+ * at README every time the number of current icons is more than `updateRange`
+ * more than the previous milestone.
+ */
+
+const fs = require('fs');
+const path = require('path');
+
+const regexMatcher = /Over\s(\d+)\s/;
+const updateRange = 500;
+
+const rootDir = path.resolve(__dirname, '..', '..');
+const dataFile = path.resolve(rootDir, '_data', 'simple-icons.json');
+const readmeFile = path.resolve(rootDir, 'README.md');
+const readmeContent = fs.readFileSync(readmeFile).toString();
+
+let overNIconsInReadme;
+try {
+  overNIconsInReadme = parseInt(regexMatcher.exec(readmeContent)[1]);
+} catch (err) {
+  console.err(
+      'Failed to obtain number of SVG icons of current milestone in README:',
+      err,
+  );
+  process.exit(1);
+}
+
+const nIcons = require(dataFile).icons.length;
+if (nIcons <= overNIconsInReadme + 500) {
+    process.exit(0);
+}
+
+const newContent = readmeContent.replace(
+  regexMatcher, `Over ${overNIconsInReadme + updateRange} `,
+);
+fs.writeFileSync(readmeFile, newContent);
