89780102ba0f8dd9652703a1623982869fe3e252
Jorge Amado Soria Ramirez <darksoul.uci@gmail.com>
2021-12-29 13:50:42 -0600

    restored test scripts

---
+++ tests/index.test.js
@@ -0,0 +1,32 @@
+import simpleIcons from '../index.js';
+import { getIconSlug, getIconsData } from '../scripts/utils.js';
+import { test, exec } from 'uvu';
+import * as assert from 'uvu/assert';
+
+(async () => {
+  const icons = await getIconsData();
+
+  icons.forEach((icon) => {
+    const slug = getIconSlug(icon);
+
+    test(`'Get' ${icon.title} by its slug`, () => {
+      const found = simpleIcons.Get(slug);
+      assert.ok(found);
+      assert.is(found.title, icon.title);
+      assert.is(found.hex, icon.hex);
+      assert.is(found.source, icon.source);
+    });
+  });
+
+  test(`Iterating over simpleIcons only exposes icons`, () => {
+    const iconArray = Object.values(simpleIcons);
+    for (let icon of iconArray) {
+      assert.ok(icon);
+      assert.type(icon, 'object');
+    }
+  });
+
+  test.run();
+
+  exec();
+})();
