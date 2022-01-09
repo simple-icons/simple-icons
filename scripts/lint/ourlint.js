809863ecaa2eb2ca5dac3f3685f3aa91b8fc7d37
Sachin Raja <sachinraja2349@gmail.com>
2022-01-09 13:42:12 -0800

    resolve conflicts

---
+++ scripts/lint/ourlint.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+/**
+ * @fileoverview
+ * Linters for the package that can't easily be implemented in the existing
+ * linters (e.g. jsonlint/svglint).
+ */
+
+import fakeDiff from 'fake-diff';
+import { getIconsDataString } from '../utils.js';
+
+/**
+ * Contains our tests so they can be isolated from each other.
+ * @type {{[k:string]: () => (string|undefined)}}
+ */
+const TESTS = {
+  /* Tests whether our icons are in alphabetical order */
+  alphabetical: (data) => {
+    const collector = (invalidEntries, icon, index, array) => {
+      if (index > 0) {
+        const prev = array[index - 1];
+        const compare = icon.title.localeCompare(prev.title);
+        if (compare < 0) {
+          invalidEntries.push(icon);
+        } else if (compare === 0) {
+          if (prev.slug) {
+            if (!icon.slug || icon.slug.localeCompare(prev.slug) < 0) {
+              invalidEntries.push(icon);
+            }
+          }
+        }
+      }
+      return invalidEntries;
+    };
+    const format = (icon) => {
+      if (icon.slug) {
+        return `${icon.title} (${icon.slug})`;
+      }
+      return icon.title;
+    };
+
+    const invalids = data.icons.reduce(collector, []);
+    if (invalids.length) {
+      return `Some icons aren't in alphabetical order:
+        ${invalids.map((icon) => format(icon)).join(', ')}`;
+    }
+  },
+
+  /* Check the formatting of the data file */
+  prettified: async (data, dataString) => {
+    const normalizedDataString = dataString.replace(/\r\n/g, '\n');
+    const dataPretty = `${JSON.stringify(data, null, '    ')}\n`;
+
+    if (normalizedDataString !== dataPretty) {
+      const dataDiff = fakeDiff(normalizedDataString, dataPretty);
+      return `Data file is formatted incorrectly:\n\n${dataDiff}`;
+    }
+  },
+};
+
+// execute all tests and log all errors
+(async () => {
+  const dataString = await getIconsDataString();
+  const data = JSON.parse(dataString);
+
+  const errors = (
+    await Promise.all(
+      Object.keys(TESTS).map((test) => TESTS[test](data, dataString)),
+    )
+  ).filter(Boolean);
+
+  if (errors.length > 0) {
+    errors.forEach((error) => console.error(`\u001b[31m${error}\u001b[0m`));
+    process.exit(1);
+  }
+})();
--- tests/index.test.js
+++ tests/index.test.js
@@ -1,90 +1,32 @@
-const { icons } = require('../_data/simple-icons.json');
-const simpleIcons = require('../index.js');
-const { getIconSlug } = require("../scripts/utils.js");
+import simpleIcons from '../index.js';
+import { getIconSlug, getIconsData } from '../scripts/utils.js';
+import { test, exec } from 'uvu';
+import * as assert from 'uvu/assert';
 
-icons.forEach(icon => {
-  const name = icon.slug || icon.title;
-  const subject = simpleIcons[name];
+(async () => {
+  const icons = await getIconsData();
 
-  test(`${icon.title} has the correct "title"`, () => {
-    expect(typeof subject.title).toBe('string');
-    expect(subject.title).toEqual(icon.title);
-  });
+  icons.forEach((icon) => {
+    const slug = getIconSlug(icon);
 
-  test(`${icon.title} has the correct "slug"`, () => {
-    expect(typeof subject.slug).toBe('string');
-    expect(subject.slug).toEqual(getIconSlug(icon));
-  });
-
-  test(`${icon.title} has the correct "hex" value`, () => {
-    expect(typeof subject.hex).toBe('string');
-    expect(subject.hex).toEqual(icon.hex);
-  });
-
-  test(`${icon.title} has the correct "source"`, () => {
-    expect(typeof subject.source).toBe('string');
-    expect(subject.source).toEqual(icon.source);
-  });
-
-  test(`${icon.title} has an "svg" value`, () => {
-    expect(typeof subject.svg).toBe('string');
-  });
-
-  test(`${icon.title} has a valid "path" value`, () => {
-    expect(typeof subject.path).toBe('string');
-    expect(subject.path).toMatch(/^[MmZzLlHhVvCcSsQqTtAaEe0-9-,.\s]+$/g);
-  });
-
-  test(`${icon.title} has ${icon.guidelines ? "the correct" : "no"} "guidelines"`, () => {
-    if (icon.guidelines) {
-      expect(typeof subject.guidelines).toBe('string');
-      expect(subject.guidelines).toEqual(icon.guidelines);
-    } else {
-      expect(subject.guidelines).toBeUndefined();
-    }
-  });
-
-  test(`${icon.title} has ${icon.license ? "the correct" : "no"} "license"`, () => {
-    if (icon.license) {
-      expect(typeof subject.license).toBe('object');
-      expect(subject.license).toHaveProperty('type', icon.license.type);
-      if (icon.license.type === "custom") {
-        expect(subject.license).toHaveProperty('url', icon.license.url);
-      } else {
-        expect(typeof subject.license.url).toBe('string');
-        expect(subject.license.url).toMatch(/^https?:\/\/[^\s]+$/);
-      }
-    } else {
-      expect(subject.license).toBeUndefined();
-    }
-  });
-
-  // NOTE: Icons with custom slugs have a custom slug because their title is
-  // already taken, so they should not be findable by their title.
-  if (icon.slug === undefined) {
-    test(`${icon.title} can be found by it's title`, () => {
-      const found = simpleIcons.get(icon.title);
-      expect(found).toBeDefined();
-      expect(found.title).toEqual(icon.title);
-      expect(found.hex).toEqual(icon.hex);
-      expect(found.source).toEqual(icon.source);
+    test(`'Get' ${icon.title} by its slug`, () => {
+      const found = simpleIcons.Get(slug);
+      assert.ok(found);
+      assert.is(found.title, icon.title);
+      assert.is(found.hex, icon.hex);
+      assert.is(found.source, icon.source);
     });
-  }
-
-  test(`${icon.title} can be found by it's slug`, () => {
-    const name = getIconSlug(icon);
-    const found = simpleIcons.get(name);
-    expect(found).toBeDefined();
-    expect(found.title).toEqual(icon.title);
-    expect(found.hex).toEqual(icon.hex);
-    expect(found.source).toEqual(icon.source);
   });
-});
 
-test(`Iterating over simpleIcons only exposes icons`, () => {
-  const iconArray = Object.values(simpleIcons);
-  for (let icon of iconArray) {
-    expect(icon).toBeDefined();
-    expect(typeof icon).toBe('object');
-  }
-});
+  test(`Iterating over simpleIcons only exposes icons`, () => {
+    const iconArray = Object.values(simpleIcons);
+    for (let icon of iconArray) {
+      assert.ok(icon);
+      assert.type(icon, 'object');
+    }
+  });
+
+  test.run();
+
+  exec();
+})();
