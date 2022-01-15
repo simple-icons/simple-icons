623b4da6b280d404ee66ad13ffb2848949a31a1f
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-01-15 22:59:03 +0100

    Declarate with `const` all variables

--- tests/readme-icons.test.js
+++ tests/readme-icons.test.js
@@ -4,20 +4,20 @@ import { fileURLToPath } from 'node:url';
 import { test } from 'uvu';
 import * as assert from 'uvu/assert';
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url)),
-  root = path.dirname(__dirname),
-  blackIconsPath = path.join(root, 'icons'),
-  whiteIconsPath = path.join(root, 'assets', 'readme'),
-  whiteIconsFileNames = fs.readdirSync(whiteIconsPath);
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const root = path.dirname(__dirname);
+const blackIconsPath = path.join(root, 'icons');
+const whiteIconsPath = path.join(root, 'assets', 'readme');
+const whiteIconsFileNames = fs.readdirSync(whiteIconsPath);
 
 for (let whiteIconFileName of whiteIconsFileNames) {
-  const whiteIconPath = path.join(whiteIconsPath, whiteIconFileName),
-    blackIconPath = path.join(
-      blackIconsPath,
-      whiteIconFileName.replace(/-white\.svg$/, '.svg'),
-    ),
-    whiteIconRelPath = path.relative(root, whiteIconPath),
-    blackIconRelPath = path.relative(root, blackIconPath);
+  const whiteIconPath = path.join(whiteIconsPath, whiteIconFileName);
+  const blackIconPath = path.join(
+    blackIconsPath,
+    whiteIconFileName.replace(/-white\.svg$/, '.svg'),
+  );
+  const whiteIconRelPath = path.relative(root, whiteIconPath);
+  const blackIconRelPath = path.relative(root, blackIconPath);
 
   test(`'${whiteIconRelPath}' content must be equivalent to '${blackIconRelPath}' content`, () => {
     assert.ok(
@@ -32,8 +32,8 @@ for (let whiteIconFileName of whiteIconsFileNames) {
         ` not found in '${path.dirname(blackIconRelPath)}' directory.`,
     );
 
-    const whiteIconContent = fs.readFileSync(whiteIconPath, 'utf8'),
-      blackIconContent = fs.readFileSync(blackIconPath, 'utf8');
+    const whiteIconContent = fs.readFileSync(whiteIconPath, 'utf8');
+    const blackIconContent = fs.readFileSync(blackIconPath, 'utf8');
     assert.equal(
       whiteIconContent,
       blackIconContent.replace('<svg', '<svg fill="white"'),
