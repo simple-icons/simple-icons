9c1fa636c83ec9b4240b4cf3f7df5ab1f6964b6f
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2022-01-30 21:38:05 +0100

    Remove uneeded import

--- tests/readme.test.js
+++ tests/readme.test.js
@@ -1,6 +1,5 @@
 import fs from 'node:fs';
 import path from 'node:path';
-import { fileURLToPath } from 'node:url';
 import { describe, test } from 'mocha';
 import { strict as assert } from 'node:assert';
 import {
