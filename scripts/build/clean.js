5b615cc88d141df032b9d83f00200ff68fe762cc
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2024-03-15 13:20:18 +0100

    Skip non existent files

--- scripts/build/clean.js
+++ scripts/build/clean.js
@@ -3,7 +3,7 @@
  * Clean files built by the build process.
  */
 
-import fs from 'node:fs/promises';
+import fs from 'node:fs';
 import path from 'node:path';
 import { getDirnameFromImportMeta } from '../../sdk.mjs';
 
@@ -11,9 +11,19 @@ const __dirname = getDirnameFromImportMeta(import.meta.url);
 const rootDirectory = path.resolve(__dirname, '..', '..');
 const files = ['index.js', 'index.mjs', 'index.d.ts', 'sdk.js'];
 
+const fileExists = (fpath) =>
+  new Promise((r) => fs.access(fpath, fs.constants.F_OK, (e) => r(!e)));
+
 Promise.all(
-  files.map((file) => fs.unlink(path.join(rootDirectory, file))),
+  files.map(async (file) => {
+    const filepath = path.join(rootDirectory, file);
+    if (!(await fileExists(filepath))) {
+      console.error(`File ${file} does not exist, skipping...`);
+      return;
+    }
+    return fs.promises.unlink(filepath);
+  }),
 ).catch((error) => {
   console.error(`Error cleaning files: ${error.message}`);
-  process.exit(0);
+  process.exit(1);
 });
