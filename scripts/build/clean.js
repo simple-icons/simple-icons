35a7166b6fa5052b89e40356f4b6fd8baee476b1
Álvaro Mondéjar Rubio <mondejar1994@gmail.com>
2024-03-15 12:38:36 +0100

    Make paths absolute

--- scripts/build/clean.js
+++ scripts/build/clean.js
@@ -4,12 +4,17 @@
  */
 
 import fs from 'node:fs/promises';
+import path from 'node:path';
+import { getDirnameFromImportMeta } from '../../sdk.mjs';
+
+const __dirname = getDirnameFromImportMeta(import.meta.url);
+const rootDirectory = path.resolve(__dirname, '..', '..');
 
 Promise.all([
-  fs.unlink('index.js'),
-  fs.unlink('index.mjs'),
-  fs.unlink('index.d.ts'),
-  fs.unlink('sdk.js'),
+  fs.unlink(path.join(rootDirectory, 'index.js')),
+  fs.unlink(path.join(rootDirectory, 'index.mjs')),
+  fs.unlink(path.join(rootDirectory, 'index.d.ts')),
+  fs.unlink(path.join(rootDirectory, 'sdk.js')),
 ]).catch((error) => {
   console.error(`Error cleaning files: ${error.message}`);
   process.exit(1);
