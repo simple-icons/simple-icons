bf69b6dee06b2a25d5e66f8bef2af2dae0fd7ce1
LitoMore <LitoMore@users.noreply.github.com>
2024-03-25 01:38:18 +0800

    Add XO linter (#10643)

--- tests/test-icon.js
+++ tests/test-icon.js
@@ -1,7 +1,7 @@
+import {strict as assert} from 'node:assert';
 import fs from 'node:fs/promises';
 import path from 'node:path';
-import { strict as assert } from 'node:assert';
-import { describe, it } from 'mocha';
+import {describe, it} from 'mocha';
 import {
   SVG_PATH_REGEX,
   URL_REGEX,
@@ -9,7 +9,7 @@ import {
   titleToSlug,
 } from '../sdk.mjs';
 
-const iconsDir = path.resolve(
+const iconsDirectory = path.resolve(
   getDirnameFromImportMeta(import.meta.url),
   '..',
   'icons',
@@ -26,7 +26,7 @@ const iconsDir = path.resolve(
  * @param {String} slug Icon data slug
  */
 export const testIcon = (icon, subject, slug) => {
-  const svgPath = path.resolve(iconsDir, `${slug}.svg`);
+  const svgPath = path.resolve(iconsDirectory, `${slug}.svg`);
 
   describe(icon.title, () => {
     it('has the correct "title"', () => {
@@ -81,7 +81,7 @@ export const testIcon = (icon, subject, slug) => {
     });
 
     if (icon.slug) {
-      // if an icon data has a slug, it must be different to the
+      // If an icon data has a slug, it must be different to the
       // slug inferred from the title, which prevents adding
       // unnecessary slugs to icons data
       it(`'${icon.title}' slug must be necessary`, () => {
