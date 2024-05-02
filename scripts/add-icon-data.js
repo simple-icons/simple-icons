c9f9494eda72e99bcc3896fc406679edfa78aefb
LitoMore <LitoMore@users.noreply.github.com>
2024-05-03 04:54:43 +0800

    Update XO configurations (#10859)

--- scripts/add-icon-data.js
+++ scripts/add-icon-data.js
@@ -121,9 +121,10 @@ try {
       : undefined,
   };
 
-  console.log(
+  process.stdout.write(
     'About to write the following to simple-icons.json:\n' +
-      JSON.stringify(answers, null, 4),
+      JSON.stringify(answers, null, 4) +
+      '\n',
   );
 
   if (
@@ -134,14 +135,14 @@ try {
     iconsData.icons.push(answers);
     iconsData.icons.sort((a, b) => collator.compare(a.title, b.title));
     await writeIconsData(iconsData);
-    console.log(chalk.green('\nData written successfully.'));
+    process.stdout.write(chalk.green('\nData written successfully.\n'));
   } else {
-    console.log(chalk.red('\nAborted.'));
+    process.stdout.write(chalk.red('\nAborted.\n'));
     process.exit(1);
   }
 } catch (error) {
   if (error instanceof ExitPromptError) {
-    console.log(chalk.red('\nAborted.'));
+    process.stdout.write(chalk.red('\nAborted.\n'));
     process.exit(1);
   }
 
