f46591393e7d3eb1043d0922607f8544f0ef8b7c
LitoMore <litomore@gmail.com>
2024-12-18 04:39:42 +0800

    Use tabs

--- scripts/release/discord-release-message.js
+++ scripts/release/discord-release-message.js
@@ -12,16 +12,16 @@ const discordReleasesWebhookUrl = process.env.DISCORD_RELEASES_WEBHOOK_URL;
 const githubReleaseUrl = `https://github.com/simple-icons/simple-icons/releases/tag/${releaseVersion}`;
 
 try {
-  await globalThis.fetch(discordReleasesWebhookUrl, {
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      content: `<@&${discordReleasesRoleId}> ${githubReleaseUrl}`,
-    }),
-  });
+	await globalThis.fetch(discordReleasesWebhookUrl, {
+		method: 'post',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify({
+			content: `<@&${discordReleasesRoleId}> ${githubReleaseUrl}`,
+		}),
+	});
 } catch {
-  console.error('Failed to send release message to Discord.');
-  process.exit(1);
+	console.error('Failed to send release message to Discord.');
+	process.exit(1);
 }
