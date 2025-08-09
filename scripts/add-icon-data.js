f4faae21b6bb5aac6c778a4a938fd270a81b9363
LitoMore <litomore@gmail.com>
2025-08-09 18:29:59 +0800

    Fix `slug` field in `add-icon-data` script

--- scripts/add-icon-data.js
+++ scripts/add-icon-data.js
@@ -111,11 +111,11 @@ const previewHexColor = (input) => {
 };
 
 /** @type {IconData} */
+// @ts-expect-error: `slug` is not required in our source simple-icons.json file.
 const answers = {
 	title: '',
 	hex: '',
 	source: '',
-	slug: '',
 };
 
 answers.title = await input({
